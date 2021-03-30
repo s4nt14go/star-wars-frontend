a1e40cfb9dc0c50540e6da5f66409ab8346b0fc9
s4nt14go <santiagopergamo@gmail.com>
2021-03-30 11:26:18 -0300

    feat: improve chewbacca image responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,8 +9,8 @@ function Header(){
   return <section className="text-gray-600 body-font">
     <div className="flex flex-col text-center w-full mb-4">
       <div>
-      <img alt='Chewbacca' src={chewbacca} className='mx-auto w-1/5 inline' onLoad={() => setImgLoaded(true)}/>
-      { !imgLoaded && <img alt='Chewbacca' src={placeholder} className='mx-auto w-1/5 inline' /> }
+      <img alt='Chewbacca' src={chewbacca} className='mx-auto inline md:w-28 w-1/4 my-4' onLoad={() => setImgLoaded(true)}/>
+      { !imgLoaded && <img alt='Chewbacca' src={placeholder} className='mx-auto inline md:w-28 w-1/4 my-4' /> }
       </div>
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">List Star Wars characters</h1>
       <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Sourcecode in <a href="https://github.com/s4nt14go/star-wars-frontend" target='_blank' rel="noreferrer" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>this repo</a></p>
