5c1d822492b061325d9d385af9bf5c37df941d75
s4nt14go <santiagopergamo@gmail.com>
2021-04-12 08:43:59 -0300

    feat: add error details
    Add react-syntax-highlighter to show JSON error

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Search from "./components/Search";
 import Table from "./components/Table";
 import {Route, Switch} from "react-router-dom";
 import Character from "./components/Character";
+import SyntaxHighlighter from 'react-syntax-highlighter';
 
 const FIELDS = gql`
     fragment Fields on PeoplePage {
@@ -162,9 +163,12 @@ function App() {
         <Header />
 
         {
-          anyError ?
-            <p>Error: {anyError.toString()}</p>
-            :
+          anyError ? <>
+              <p>Error received! The <a href="https://github.com/s4nt14go/star-wars-backend" target='_blank' rel="noreferrer" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>backend</a> actually is just an API proxy that GETs data from <a href="https://swapi.dev" target='_blank' rel="noreferrer" className='underline text-blue-600 hover:text-blue-800 visited:text-purple-600'>https://swapi.dev</a>, so maybe something is wrong in it:</p>
+              <SyntaxHighlighter language='json'>
+                {anyError}
+              </SyntaxHighlighter>
+            </>:
             anyLoading && !state.currResults.length ?
               <p>Loading... {anyLoading.toString}</p>
               :
